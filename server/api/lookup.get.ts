c3ebb181ed91ad8ef9a02b94607708e1326668db
Ahmed Rangel <ahmedrangel@outlook.com>
2024-11-28 01:24:08 -0500

    fix: api

--- server/api/lookup.get.ts
+++ server/api/lookup.get.ts
@@ -7,8 +7,11 @@ export default defineCachedEventHandler(async (event) => {
   if (!result) {
     throw createError({
       statusCode: ErrorCode.INTERNAL_SERVER_ERROR,
-      statusMessage: 'An error occurred while analyzing, please try again later'
+      statusMessage: "An error occurred while analyzing, please try again later"
     });
   }
   return result;
+}, {
+  maxAge: 12 * 60 * 60,
+  swr: true
 });
