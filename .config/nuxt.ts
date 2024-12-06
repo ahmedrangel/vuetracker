e5f8fbfa267c22c3f70f12ec3633ef07db38ac2e
Ahmed Rangel <ahmedrangel@outlook.com>
2024-12-06 00:32:01 -0500

    chore: no prerender sitemap

--- .config/nuxt.ts
+++ .config/nuxt.ts
@@ -72,7 +72,7 @@ export default defineNuxtConfig({
     prerender: {
       autoSubfolderIndex: false,
       crawlLinks: false,
-      routes: ["/", "/sitemap.xml"]
+      routes: ["/"]
     },
     cloudflare: {
       pages: {
