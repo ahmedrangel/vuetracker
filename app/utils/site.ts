eb0e669612ea4b746189be934d539805244e7d84
Ahmed Rangel <ahmedrangel@outlook.com>
2024-12-06 10:51:03 -0500

    chore: site add description

--- app/utils/site.ts
+++ app/utils/site.ts
@@ -1,5 +1,6 @@
 export const SITE = {
   name: "VueTracker",
   url: "https://vuetracker.nuxt.dev",
+  description: "Discover the plugins and technologies powering websites made with Vue",
   ogImage: "/images/og-card.jpg"
 };
