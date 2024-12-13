6324c29451629b9e72554153d2cc2487997ccc8c
Ahmed Rangel <ahmedrangel@outlook.com>
2024-12-13 14:17:05 -0500

    chore: remove nuxt modules from skeleton tracker details

--- app/components/Skeleton/TrackerDetails.vue
+++ app/components/Skeleton/TrackerDetails.vue
@@ -18,16 +18,6 @@
     <div class="flex flex-wrap items-start gap-2">
       <SkeletonTechCardBasic />
       <SkeletonTechCardBasic />
-    </div>
-  </div>
-  <div class="flex flex-col gap-5">
-    <div class="flex items-center gap-2 text-primary-600 dark:text-primary-400 font-bold tracking-tight">
-      <Icon name="fa6-solid:cubes" size="2rem" />
-      <p>NUXT MODULES</p>
-    </div>
-    <div class="flex flex-wrap items-start gap-2">
-      <SkeletonTechCardBasic />
-      <SkeletonTechCardBasic />
       <SkeletonTechCardBasic />
     </div>
   </div>
