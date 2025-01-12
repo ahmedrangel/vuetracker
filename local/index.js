8a3f45c65ba6df32ece0a64bd6dec3d3529864b4
Ahmed Rangel <ahmedrangel@outlook.com>
2025-01-12 12:12:48 -0500

    feat: include analyzer proxy code in main repo

---
+++ local/index.js
@@ -0,0 +1,25 @@
+import { createServer } from "node:http";
+import { createServerAdapter } from "@whatwg-node/server";
+import { AutoRouter } from "itty-router";
+import { analyze } from "vuetracker-analyzer";
+
+const router = AutoRouter();
+
+router.get("/analyze?", async (req) => {
+  const { url } = req.query;
+  return await analyze(url, {
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 VueTracker/1.0 (Debian GNU/Linux 12; arm64; +https://vuetracker.nuxt.dev/)",
+    executablePath: "/usr/bin/chromium-browser"
+  }).catch((e) => {
+    return Response.json({ success: false, error: { message: e.message, cause: e.cause } }, { status: 500 });
+  });
+});
+
+router.all("*", () =>
+  Response.json({ success: false, error: "Route not found" }, { status: 404 })
+);
+
+const ittyServer = createServerAdapter(router.fetch);
+const httpServer = createServer(ittyServer);
+httpServer.listen(2082);
+console.info("Server running on http://localhost:2082");
