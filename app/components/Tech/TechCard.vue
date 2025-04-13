84c3f650599e0a2734baa5cbc911b0c167d760b0
Ahmed Rangel <ahmedrangel@outlook.com>
2025-04-13 16:12:43 -0500

    chore: adjust tech card icon size

--- app/components/Tech/TechCard.vue
+++ app/components/Tech/TechCard.vue
@@ -10,7 +10,7 @@ defineProps<{
   <div class="flex flex-col w-full bg-neutral-200 dark:bg-neutral-800 border-2 border-neutral-300 dark:border-neutral-700 rounded-xl overflow-hidden transition">
     <h6 class="self-start bg-neutral-300 dark:bg-neutral-700 px-3 py-1 rounded-br-xl rounded-bl-none rounded-tr-none text-xs tracking-tight uppercase leading-sm font-bold">{{ title }}</h6>
     <div class="flex justify-center items-center px-2 py-3 gap-1">
-      <Icon v-if="icon" :name="'vuetracker:' + icon" size="1.8rem" />
+      <Icon v-if="icon" :name="'vuetracker:' + icon" height="1.8em" width="1.8em" />
       <p class="text-base font-bold tracking-tight">{{ value }}</p>
     </div>
   </div>
