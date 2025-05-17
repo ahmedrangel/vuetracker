3c4f5d62459e4d0932857576d44b4dffe20b9282
Ahmed Rangel <ahmedrangel@outlook.com>
2025-05-17 17:11:34 -0500

    chore: update domain

--- app/utils/site.ts
+++ app/utils/site.ts
@@ -1,6 +1,6 @@
 export const SITE = {
   name: "VueTracker",
-  url: "https://vuetracker.nuxt.dev",
+  url: "https://vuetracker.pages.dev",
   description: "Discover the plugins and technologies powering websites made with Vue",
   ogImage: "/images/og-card.jpg"
 };
