2ee209d82102a0cf8dd0940c0634308e4d34193b
Ahmed Rangel <ahmedrangel@outlook.com>
2025-05-20 11:04:56 -0500

    chore: use updated at for sitemap lastmod

--- server/api/__sitemap.ts
+++ server/api/__sitemap.ts
@@ -2,12 +2,13 @@ export default defineEventHandler(async () => {
   const routeRules = [] as { loc: string, lastmod: string }[];
   const DB = useDB();
   const sites = await DB.select({
-    url: tables.sites.url
+    url: tables.sites.url,
+    updatedAt: tables.sites.updatedAt,
   }).from(tables.sites).all();
   routeRules.push({ loc: "/explore", lastmod: new Date().toISOString() });
 
   for (const site of sites) {
-    routeRules.push({ loc: normalizeSITE(site.url), lastmod: new Date().toISOString() });
+    routeRules.push({ loc: normalizeSITE(site.url), lastmod: new Date(site.updatedAt).toISOString() });
   }
   return routeRules;
 });
