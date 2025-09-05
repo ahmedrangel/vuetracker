b3e86040469f85478be8191f140734a92d66049b
Ahmed Rangel <ahmedrangel@outlook.com>
2025-09-05 18:51:18 -0500

    chore: adjust

--- local/index.js
+++ local/index.js
@@ -1,20 +1,19 @@
 import { createServer } from "node:http";
-import { loadEnvFile } from "node:process";
+import { platform } from "node:process";
 import { createServerAdapter } from "@whatwg-node/server";
 import { AutoRouter } from "itty-router";
 import { analyze } from "vuetracker-analyzer";
 import { $fetch } from "ofetch";
 
-loadEnvFile();
 const router = AutoRouter();
 
-const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 VueTracker/1.0 (Debian GNU/Linux 12; arm64; +vuetracker.pages.dev)";
+const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36 VueTracker/1.0 (Debian GNU/Linux 12; arm64; +vuetracker.pages.dev)";
 
 router.get("/analyze?", async (req) => {
   const { url } = req.query;
   return await analyze(url, {
     userAgent,
-    executablePath: process.env.NUXT_ANALYZER_PROXY_OS === "windows" ? undefined : "/usr/bin/chromium"
+    ...platform === "linux" && { executablePath: "/usr/bin/chromium" }
   }).catch((e) => {
     return Response.json({ success: false, error: { message: e.message, cause: e.cause } }, { status: 500 });
   });
