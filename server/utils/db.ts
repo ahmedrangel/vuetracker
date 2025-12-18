446d40f24884284cae267e4c74f4cba713c67790
Ahmed Rangel <ahmedrangel@outlook.com>
2025-12-18 03:24:21 -0500

    Update db.ts

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/d1";
 import { db, schema } from "hub:db";
-import * as dbTables from "../database/schema";
+import * as dbTables from "../db/schema";
 
 export { sql, eq, and, or, count, desc, asc, exists, notExists } from "drizzle-orm";
 export const tables = dbTables;
